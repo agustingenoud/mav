02a8384d7d3de4225cbf326bbe96ea04c56e4439
agustingenoud <agustingenoud@gmail.com>
2021-09-16 11:14:10 -0300

    G0

---
+++ index.js
@@ -0,0 +1,91 @@
+// SERVER CODE
+
+const express = require("express");
+const { google } = require("googleapis");
+
+const app = express();
+
+const auth = new google.auth.GoogleAuth({
+  keyFile: "credentials.json",
+  scopes: "https://www.googleapis.com/auth/spreadsheets",
+});
+
+app.listen(3000, () => console.log("listenin at 3000-tremil"));
+app.use(express.static("public")); // public folder to serve
+app.use(
+  express.json({
+    limit: "1mb",
+  })
+);
+/* 
+app.get("/api", (request, response) => {
+  database.find({}, (err, data) => {
+    if (err) {
+      response.end();
+      return;
+    }
+    response.json(data);
+  });
+});
+ */
+// REQUEST API
+app.post("/api", async (request, response) => {
+  const in_btn = request.body.btn;
+  console.log(` * LALALA * ${JSON.stringify(in_btn)} `);
+
+  // googleapis--------------------------------
+  //create client instance for gAuth
+  const client = await auth.getClient();
+  const googleSheets = google.sheets({
+    version: "v4",
+    auth: client,
+  });
+  const spreadsheetId = "1ZRU1TZ7rCGvHQxzRA6xRlzFCXZqsM8BKySNLgH95JYM";
+  let getRows = "";
+
+  switch (in_btn) {
+    case "TV":
+      console.log(`padentro del case 1: ${in_btn}`);
+      getRows = await googleSheets.spreadsheets.values.get({
+        auth,
+        spreadsheetId,
+        range: "tv!A:H",
+        majorDimension: "COLUMNS",
+      });
+
+      break;
+    case "Cine":
+      console.log('Ahora "cine" está adentro del case AGREGAR');
+      getRows = await googleSheets.spreadsheets.values.get({
+        auth,
+        spreadsheetId,
+        range: "tv!A:H",
+        majorDimension: "COLUMNS",
+      });
+      break;
+    case "Publicidad":
+      console.log('Ahora "publicidad" está adentro del case PUBLICIDAD');
+      getRows = await googleSheets.spreadsheets.values.get({
+        auth,
+        spreadsheetId,
+        range: "tv!A:H",
+        majorDimension: "COLUMNS",
+      });
+      break;
+    case "Agregar":
+      console.log('Ahora "agregar" está adentro del case AGREGAR');
+      getRows = await googleSheets.spreadsheets.values.get({
+        auth,
+        spreadsheetId,
+        range: "tv!A:H",
+        majorDimension: "COLUMNS",
+      });
+      break;
+  }
+
+  console.log(getRows); // From switch
+  const data = getRows;
+  // Fin --- googleapis
+
+  response.json(data);
+});
---
+++ public/sketch.js
@@ -0,0 +1,98 @@
+"use strict";
+
+//const btns = document.querySelectorAll("button");
+const btns = document.querySelectorAll(".rubro");
+const asig_ref = document.querySelector("#sel_asignacion_id");
+const tiempo_ref = document.querySelector("#sel_tiempo_id");
+const btn_agregar = document.querySelector("#btn_agregar_id");
+const planilla = document.querySelector(".planilla");
+
+for (let item of btns) {
+  item.addEventListener("click", async () => {
+    console.log(`Apretaste el botón: ${item.innerHTML}`);
+
+    const data_srv = { btn: item.innerHTML };
+    const boton = item.innerHTML;
+
+    const options = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data_srv),
+    };
+
+    const response = await fetch("/api", options); // SEND-api
+
+    //----------//
+
+    const input_json = await response.json(); /* .catch(function (err) {
+      console.error(err);
+    }); // RECEIVE-api */
+    const received = input_json.data.values;
+
+    // Value pair > {"base_comparativa": "salario"}
+    const compare = {};
+    for (let i = 0; i < received[2].length; i++) {
+      let key = received[2][i];
+      let value = received[3][i];
+      compare[key] = value;
+    }
+
+    // Completar SELECT > OPTION
+    let menu_item = 0;
+    for (let arr of received) {
+      // Container p-llenar campos de selects
+      var selasig_container = document.querySelector("#sel_asignacion_id");
+      menu_item++;
+      arr.forEach((item) => {
+        if (menu_item == 2) {
+          selasig_container = document.querySelector("#sel_tiempo_id");
+          const opt = document.createElement("option");
+          opt.id = "";
+          opt.innerHTML = `<option> ${item} </option>`;
+          selasig_container.appendChild(opt);
+        } else if (menu_item == 1) {
+          selasig_container = document.querySelector("#sel_asignacion_id");
+          const opt = document.createElement("option");
+          opt.id = "";
+          opt.innerHTML = `<option> ${item} </option>`;
+          selasig_container.appendChild(opt);
+        }
+      });
+    }
+
+    // Agregar línea a planilla y campos
+    let num = 0;
+    let totalItems = 0;
+    btn_agregar.addEventListener("click", () => {
+      const query = `${asig_ref.value}${tiempo_ref.value}`;
+      let salario = compare[query];
+
+      let linea_contrato = {
+        produccion: boton,
+        asignacion: asig_ref.value,
+        tiempo: tiempo_ref.value,
+        salario: Number(
+          salario.replace(/[\,]/g, (m) => {
+            return m == "," ? "." : ".";
+          })
+        ),
+      };
+      console.log(`Línea para la planilla ${JSON.stringify(linea_contrato)}`);
+
+      const itemPlanilla = document.createElement("tr");
+      itemPlanilla.className = "seconds";
+      itemPlanilla.innerHTML = `<td class="td-uno">${linea_contrato.asignacion}</td>
+                                <td class="td-dos">${linea_contrato.tiempo}</td>
+                                <td class="td-tres">${linea_contrato.salario}</td>`;
+      planilla.appendChild(itemPlanilla);
+
+      num = linea_contrato.salario;
+      totalItems += num;
+      document.querySelector("#total").innerHTML = `Total: ${parseFloat(
+        totalItems
+      ).toFixed(2)}`;
+    });
+  }); // Fin item.addEventListener
+} // Fin for(let item of btns)
