92380e6bd715de648ae77331adbfbac8e57f761d
agustingenoud <agustingenoud@gmail.com>
2021-10-04 13:39:03 -0300

    entrega 1

--- public/sketch.js
+++ public/sketch.js
@@ -32,6 +32,8 @@ for (let item of btns) {
   item.addEventListener("click", async () => {
     console.log(`Apretaste el botón: ${item.innerHTML}`);
     boton = item.innerHTML;
+
+    // Revisar borrar-todo cuándo aprieto botón
     const clean = () => {
       prod_ref.options.length = 0;
       cont_ref.options.length = 0;
@@ -58,14 +60,6 @@ for (let item of btns) {
     received = input_json.data.values;
     //console.log(received);
 
-    // Value pair > {"base_comparativa": "salario"}
-    const compare = {};
-    for (let i = 0; i < received[2].length; i++) {
-      let key = received[2][i];
-      let value = received[3][i];
-      compare[key] = value;
-    }
-
     // Asignacion menu producciones
     let selected = [];
     for (let step = 0; step < received[0].length; step++) {
@@ -82,10 +76,36 @@ for (let item of btns) {
 
     // Fill menu con DISTINCT(resultados)
     for (let step = 0; step < selected_unique.length; step++) {
+      if (step == 0) {
+        // Producción
+        selasig_container = document.querySelector("#sel_produccion_id");
+        let opt = document.createElement("option");
+        opt.id = "prod-head";
+        opt.innerText = `Tipo de producción`;
+        selasig_container.appendChild(opt);
+        // Personal
+        selasig_container = document.querySelector("#sel_cont_id");
+        opt = document.createElement("option");
+        opt.id = "cont-head";
+        opt.innerText = `Categoría de personal`;
+        selasig_container.appendChild(opt);
+        // Función
+        selasig_container = document.querySelector("#sel_asignacion_id");
+        opt = document.createElement("option");
+        opt.id = "cont-head";
+        opt.innerText = `Función`;
+        selasig_container.appendChild(opt);
+        // Tipo de contratación
+        selasig_container = document.querySelector("#sel_tiempo_id");
+        opt = document.createElement("option");
+        opt.id = "cont-head";
+        opt.innerText = `Tipo de contratación`;
+        selasig_container.appendChild(opt);
+      }
       selasig_container = document.querySelector("#sel_produccion_id");
       const opt = document.createElement("option");
       opt.id = "";
-      opt.innerHTML = `<option> ${selected_unique[step]} </option>`;
+      opt.innerText = `${selected_unique[step]}`;
       selasig_container.appendChild(opt);
     }
   }); // Fin item.addEventListener
@@ -99,27 +119,45 @@ prod_ref.addEventListener("click", () => {
 
   // Asignacion menu cont
   for (let step = 0; step < received[1].length; step++) {
+    if (step == 0) {
+      // Personal
+      selasig_container = document.querySelector("#sel_cont_id");
+      const opt = document.createElement("option");
+      opt.id = "cont-head";
+      opt.innerText = `Categoría de personal`;
+      selasig_container.appendChild(opt);
+    }
     if (received[1][step] == prod_seleccion) {
       selasig_container = document.querySelector("#sel_cont_id");
       console.log(received[1][step]);
       const opt = document.createElement("option");
       opt.id = "";
-      opt.innerHTML = `<option> ${received[2][step]} </option>`;
+      opt.innerText = `${received[2][step]}`;
       selasig_container.appendChild(opt);
     }
   }
+  console.log(
+    `FIND POSITION > received[1] ${received[1]} \n ---- \n prod_seleccion ${prod_seleccion} `
+  );
   sel_pos_prod = findPositions(received[1], prod_seleccion);
 });
 
 // Seleccion cont
 cont_ref.addEventListener("click", async () => {
-  console.log(`CLICK en asi! con el valor ${cont_ref.value}`);
+  console.log(`CLICK en cont! con el valor ${cont_ref.value}`);
 
   asig_ref.options.length = 0; // Limpio campo inferior
 
-  let ind = sel_pos_prod[cont_ref.selectedIndex];
-  console.log("----El CONVENIO-----");
-  let cnv = received[3][ind];
+  let ind = sel_pos_prod[cont_ref.selectedIndex - 1];
+
+  console.log(`----El CONVENIO----- ind: ${ind}
+  \n ------ \n
+  sel_pos_prod \n ${sel_pos_prod}
+  \n ------ \n 
+  sel_pos_prod[cont_ref.selectedIndex - 1] \n
+  ${sel_pos_prod[cont_ref.selectedIndex - 1]}`);
+
+  let cnv = received[3][ind]; // el convenio cnv
   console.log(cnv);
 
   const data_srv_conv = { convenio: cnv };
@@ -139,6 +177,13 @@ cont_ref.addEventListener("click", async () => {
   const input_json = await response.json(); // RECEIVE-apu */
   console.log(input_json);
   received_cnv = input_json.data.values;
+  console.log("received_cnv");
+  console.log(received_cnv);
+  received_cnv[0] = received_cnv[0].map((item) => item.trim());
+  received_cnv[1] = received_cnv[1].map((item) => item.trim());
+  received_cnv[2] = received_cnv[2].map((item) => item.trim());
+  received_cnv[3] = received_cnv[3].map((item) => item.trim());
+  console.log("received_cnv");
   console.log(received_cnv);
 
   // Asignacion menu producciones
@@ -157,40 +202,69 @@ cont_ref.addEventListener("click", async () => {
 
   // Fill menu con DISTINCT(resultados)
   for (let step = 0; step < selected_unique.length; step++) {
+    if (step == 0) {
+      // Función
+      selasig_container = document.querySelector("#sel_asignacion_id");
+      const opt = document.createElement("option");
+      opt.id = "cont-head";
+      opt.innerText = `Función`;
+      selasig_container.appendChild(opt);
+    }
     selasig_container = document.querySelector("#sel_asignacion_id");
     const opt = document.createElement("option");
     opt.id = "";
-    opt.innerHTML = `<option> ${selected_unique[step]} </option>`;
+    opt.innerText = `${selected_unique[step]}`;
     selasig_container.appendChild(opt);
   }
+
+  /* // revisar
   sel_pos_cont = findPositions(received_cnv[1], asig_ref.value);
   console.log("_______sel_pos_cont------");
-  //console.log(sel_pos_cont);
+  console.log(sel_pos_cont); */
 });
 
 asig_ref.addEventListener("click", async () => {
-  console.log("SALE");
+  console.log("click en asig_ref");
+
+  // revisar
+  sel_pos_cont = findPositions(received_cnv[1], asig_ref.value);
+  console.log("_______sel_pos_cont------");
+  console.log(sel_pos_cont);
+
   let asig_val = asig_ref.value;
   tiempo_ref.options.length = 0; // Limpio campo inferior
 
-  let ind = sel_pos_cont[asig_ref.selectedIndex];
+  //let ind = sel_pos_cont[asig_ref.selectedIndex];
 
   // Asignacion menu tiempo
   for (let step = 0; step < received_cnv[1].length; step++) {
+    if (step == 0) {
+      // Tipo de contratación
+      selasig_container = document.querySelector("#sel_tiempo_id");
+      const opt = document.createElement("option");
+      opt.id = "cont-head";
+      opt.innerText = `Tipo de contratación`;
+      selasig_container.appendChild(opt);
+    }
     if (received_cnv[1][step] == asig_val) {
       selasig_container = document.querySelector("#sel_tiempo_id");
       const opt = document.createElement("option");
       opt.id = "";
-      opt.innerHTML = `<option> ${received_cnv[2][step]} </option>`;
+      opt.innerText = `${received_cnv[2][step]}`;
       selasig_container.appendChild(opt);
     }
   }
 });
 
 tiempo_ref.addEventListener("click", () => {
-  let ind = sel_pos_cont[tiempo_ref.selectedIndex];
+  let ind = sel_pos_cont[tiempo_ref.selectedIndex - 1];
   monto = received_cnv[3][ind];
+  console.log(`received_cnv[3][ind]: ${received_cnv[3][ind]}`);
+  console.log(`MONTO: ${monto}`);
   //document.querySelector("#total").innerText = `TOTAL ${received_cnv[3][ind]}`;
+  document.querySelector(
+    "#subtotal"
+  ).innerText = `Subtotal ${received_cnv[3][ind]} `;
 });
 
 // Agregar línea a planilla y campos
@@ -223,7 +297,7 @@ btn_agregar.addEventListener("click", () => {
       })
   );
   totalItems += num;
-  document.querySelector("#total").innerText = `Total $ ${formatNumber(
+  document.querySelector("#total").innerText = `$ ${formatNumber(
     totalItems.toFixed(2)
   )}`;
 });
